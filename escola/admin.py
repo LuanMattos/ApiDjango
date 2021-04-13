b25a234c8a260fe84520604802d4192f51668d29
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-04-12 22:36:43 -0300

    Serializer API

--- escola/admin.py
+++ escola/admin.py
@@ -1,23 +1,19 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso, Matricula
+from escola.models import Aluno, Curso
 
 class Alunos(admin.ModelAdmin):
-    list_display = ('id','nome', 'rg', 'cpf', 'data_nascimento')
-    list_display_links = ('id', 'nome')
-    search_fields = ('nome',)
+    list_display = ('id','nome','rg','cpf','data_nascimento')
+    list_display_link = ('id','nome')
     list_per_page = 20
+    search_fields = ('nome',)
 
 admin.site.register(Aluno, Alunos)
 
 class Cursos(admin.ModelAdmin):
-    list_display = ('id', 'codigo_curso', 'descricao')
-    list_display_links = ('id', 'codigo_curso')
+    list_display = ('id','codigo_curso','descricao')
+    list_display_link = ('id','codigo_curso')
+    list_per_page = 20
     search_fields = ('codigo_curso',)
 
 admin.site.register(Curso, Cursos)
 
-class Matriculas(admin.ModelAdmin):
-    list_display = ('id', 'aluno', 'curso', 'periodo')
-    list_display_links = ('id', )
-
-admin.site.register(Matricula, Matriculas)
