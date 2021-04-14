6b08eb573bdd033c03756506e0204ffb57e485ae
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-04-13 21:55:20 -0300

    viewset de cursos

--- escola/admin.py
+++ escola/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class Alunos(admin.ModelAdmin):
     list_display = ('id','nome','rg','cpf','data_nascimento')
-    list_display_link = ('id','nome')
+    list_display_links = ('id','nome')
     list_per_page = 20
     search_fields = ('nome',)
 
@@ -11,9 +11,15 @@ admin.site.register(Aluno, Alunos)
 
 class Cursos(admin.ModelAdmin):
     list_display = ('id','codigo_curso','descricao')
-    list_display_link = ('id','codigo_curso')
+    list_display_links = ('id','codigo_curso','descricao')
     list_per_page = 20
     search_fields = ('codigo_curso',)
 
 admin.site.register(Curso, Cursos)
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id','aluno','curso','periodo')
+    list_display_links = ('id',)
+
+admin.site.register(Matricula, Matriculas)
 
