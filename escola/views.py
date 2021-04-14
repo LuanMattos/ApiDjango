e69efe32004c020964ae67026c4eb50e9488a419
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-04-13 22:44:12 -0300

    serializer subitem url

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer ,ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosCursoSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """ Exibe todos os alunos """
@@ -16,3 +16,17 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     """ Lista todas as matrículas """
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Lista as matrículas de um Aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListCreateAPIView):
+    """ Lista alunos matriculados em um curso - neste query set que pegamos o pk da requisição """
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosCursoSerializer
