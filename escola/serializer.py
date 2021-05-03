e230e1d62566ac9d37fc1c333876a1de70e8ebdf
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-05-02 22:12:03 -0300

    Upload de foto do aluno - upload de arquivos estaticos

--- escola/serializer.py
+++ escola/serializer.py
@@ -4,7 +4,7 @@ from escola.models import Aluno, Curso, Matricula
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento','foto']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,7 +31,8 @@ class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
         model = Matricula
         fields = ['aluno_nome']
 
+# -------------------- Versões 2 --------------------
 class AlunoSerializerV2(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento','celular']
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento','celular','foto']
